40934dd0669bd73b6072e1542c89cb1530c4ee45
Vinskky <naitsabesdv@gmail.com>
2020-12-05 11:35:40 +0100

    added criature module
    this will be used on enemies as inheritance for the modules

---
+++ Game/Source/Criature.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include "Collider.h"
+#include "Point.h"
+
+struct SDL_Texture;
+
+enum State
+{
+	ENEMY_IDLE,
+	ENEMY_WALK_L,
+	ENEMY_WALK_R,
+	ENEMY_FLY_L,
+	ENEMY_FLY_R,
+	DEAD
+};
+
+enum Direction
+{
+	NON,
+	ENEMY_UP,
+	ENEMY_DOWN,
+	ENEMY_LEFT,
+	ENEMY_RIGHT
+};
+
+class Criature
+{
+public:
+	enum Type
+	{
+		EN_NORMAL,
+		EN_FLY
+	};
+	Criature() {};
+	virtual ~Criature() {};
+
+	//Called before render is available
+	virtual bool Awake()
+	{
+		return true;
+	}
+
+	// Called before the first frame
+	virtual bool Start()
+	{
+		return true;
+	}
+
+	// Called each loop iteration
+	virtual bool PreUpdate()
+	{
+		return true;
+	}
+
+	// Called each loop iteration
+	virtual bool Update(float dt)
+	{
+		return true;
+	}
+
+	// Called each loop iteration
+	virtual bool PostUpdate()
+	{
+		return true;
+	}
+
+	// Called before quitting
+	virtual bool CleanUp()
+	{
+		return true;
+	}
+
+	//virtual load/save function
+	virtual bool Load(pugi::xml_node&)
+	{
+		return true;
+	}
+
+	virtual bool Save(pugi::xml_node&)const
+	{
+		return true;
+	}
+
+	virtual void OnCollision(Collisions*, Collisions*) {}
+
+private:
+
+	State enemyState;
+	Direction enemyDirection;
+
+	iPoint enemyPos;
+	iPoint enemyVel;
+
+	bool isFlying = false;
+	bool isInPlatform = false;
+
+	SString enName;
+	bool active = false;
+
+	Type type;
+	Collisions* enemyCollider = nullptr;
+
+};
+
